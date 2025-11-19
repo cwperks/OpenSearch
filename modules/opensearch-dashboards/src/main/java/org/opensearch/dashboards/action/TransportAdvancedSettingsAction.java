812c9ef4a0f00e02645a0d4a20867d17995d72e1
Craig Perkins <cwperx@amazon.com>
2025-11-18 20:06:37 -0500

    Add logging
    Signed-off-by: Craig Perkins <cwperx@amazon.com>

--- modules/opensearch-dashboards/src/main/java/org/opensearch/dashboards/action/TransportAdvancedSettingsAction.java
+++ modules/opensearch-dashboards/src/main/java/org/opensearch/dashboards/action/TransportAdvancedSettingsAction.java
@@ -8,21 +8,28 @@
 
 package org.opensearch.dashboards.action;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.opensearch.Version;
 import org.opensearch.action.get.GetRequest;
 import org.opensearch.action.index.IndexRequest;
 import org.opensearch.action.support.ActionFilters;
 import org.opensearch.action.support.HandledTransportAction;
 import org.opensearch.common.inject.Inject;
+import org.opensearch.common.time.DateFormatter;
 import org.opensearch.common.util.concurrent.ThreadContext;
 import org.opensearch.core.action.ActionListener;
 import org.opensearch.tasks.Task;
 import org.opensearch.transport.TransportService;
 import org.opensearch.transport.client.Client;
 
+import java.time.Instant;
+import java.util.List;
 import java.util.Map;
 
 public class TransportAdvancedSettingsAction extends HandledTransportAction<AdvancedSettingsRequest, AdvancedSettingsResponse> {
+
+    private static final Logger log = LogManager.getLogger(TransportAdvancedSettingsAction.class);
 
     private final Client client;
     private final String configKey;
@@ -38,8 +45,20 @@ public class TransportAdvancedSettingsAction extends HandledTransportAction<Adva
     protected void doExecute(Task task, AdvancedSettingsRequest request, ActionListener<AdvancedSettingsResponse> listener) {
         try (final ThreadContext.StoredContext ctx = client.threadPool().getThreadContext().stashContext()) {
             if (request.getSettings() != null) {
+                Map<String, Object> doc = Map.of(
+                    "type",
+                    "config",
+                    "attributes",
+                    request.getSettings(),
+                    "references",
+                    List.of(),
+                    "updated_at",
+                    DateFormatter.forPattern("strict_date_time").format(Instant.now())
+                );
+
+                log.info("Trying to index new UI Settings: " + doc);
                 // PUT operation - store settings
-                IndexRequest indexRequest = new IndexRequest(request.getIndex()).id(configKey).source(request.getSettings());
+                IndexRequest indexRequest = new IndexRequest(request.getIndex()).id(configKey).source(doc);
 
                 client.index(
                     indexRequest,
