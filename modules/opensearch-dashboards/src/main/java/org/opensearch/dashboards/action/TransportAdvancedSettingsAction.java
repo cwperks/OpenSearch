578bf3dcc471623241e48dcf173859ddce8aefb8
Craig Perkins <cwperx@amazon.com>
2025-11-20 14:09:57 -0500

    Test with multi-tenancy
    Signed-off-by: Craig Perkins <cwperx@amazon.com>

--- modules/opensearch-dashboards/src/main/java/org/opensearch/dashboards/action/TransportAdvancedSettingsAction.java
+++ modules/opensearch-dashboards/src/main/java/org/opensearch/dashboards/action/TransportAdvancedSettingsAction.java
@@ -21,11 +21,13 @@ import org.opensearch.common.time.DateFormatter;
 import org.opensearch.common.util.concurrent.ThreadContext;
 import org.opensearch.core.action.ActionListener;
 import org.opensearch.core.rest.RestStatus;
+import org.opensearch.index.IndexNotFoundException;
 import org.opensearch.tasks.Task;
 import org.opensearch.transport.TransportService;
 import org.opensearch.transport.client.Client;
 
 import java.time.Instant;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -47,28 +49,84 @@ public class TransportAdvancedSettingsAction extends HandledTransportAction<Adva
     protected void doExecute(Task task, AdvancedSettingsRequest request, ActionListener<AdvancedSettingsResponse> listener) {
         try (final ThreadContext.StoredContext ctx = client.threadPool().getThreadContext().stashContext()) {
             if (request.getSettings() != null) {
-                Map<String, Object> doc = Map.of(
-                    "type",
-                    "config",
-                    "config",
-                    request.getSettings(),
-                    "references",
-                    List.of(),
-                    "updated_at",
-                    DateFormatter.forPattern("strict_date_time").format(Instant.now())
-                );
+                // First get existing document to merge settings
+                GetRequest getRequest = new GetRequest(request.getIndex(), configKey);
 
-                log.info("Trying to index new UI Settings: " + doc);
-                // PUT operation - store settings
-                IndexRequest indexRequest = new IndexRequest(request.getIndex()).id(configKey).source(doc);
+                client.get(getRequest, ActionListener.wrap(getResponse -> {
+                    Map<String, Object> existingConfig = new HashMap<>();
 
-                client.index(
-                    indexRequest,
-                    ActionListener.wrap(
-                        indexResponse -> listener.onResponse(new AdvancedSettingsResponse(request.getSettings())),
-                        listener::onFailure
-                    )
-                );
+                    if (getResponse.isExists()) {
+                        Map<String, Object> source = getResponse.getSourceAsMap();
+                        Object config = source.get("config");
+                        if (config instanceof Map) {
+                            @SuppressWarnings("unchecked")
+                            Map<String, Object> configMap = (Map<String, Object>) config;
+                            existingConfig.putAll(configMap);
+                        }
+                    }
+
+                    // Merge new settings with existing ones
+                    existingConfig.putAll(request.getSettings());
+
+                    Map<String, Object> doc = Map.of(
+                        "type",
+                        "config",
+                        "config",
+                        existingConfig,
+                        "references",
+                        List.of(),
+                        "updated_at",
+                        DateFormatter.forPattern("strict_date_time").format(Instant.now())
+                    );
+
+                    log.info("Trying to index merged UI Settings: " + doc);
+                    IndexRequest indexRequest = new IndexRequest(request.getIndex()).id(configKey).source(doc);
+
+                    client.index(
+                        indexRequest,
+                        ActionListener.wrap(
+                            indexResponse -> listener.onResponse(new AdvancedSettingsResponse(doc)),
+                            (e) -> {
+                                System.out.println("Caught error trying to index merged UI Settings. " + e.getMessage());
+                                log.warn("Caught error trying to index merged UI Settings", e);
+                                listener.onFailure(e);
+                            }
+                        )
+                    );
+                }, (e) -> {
+                    if (e instanceof IndexNotFoundException) {
+                        // Index doesn't exist, proceed with new settings only
+                        Map<String, Object> doc = Map.of(
+                            "type",
+                            "config",
+                            "config",
+                            request.getSettings(),
+                            "references",
+                            List.of(),
+                            "updated_at",
+                            DateFormatter.forPattern("strict_date_time").format(Instant.now())
+                        );
+
+                        log.info("Index not found, creating new UI Settings: " + doc);
+                        IndexRequest indexRequest = new IndexRequest(request.getIndex()).id(configKey).source(doc);
+
+                        client.index(
+                            indexRequest,
+                            ActionListener.wrap(
+                                indexResponse -> listener.onResponse(new AdvancedSettingsResponse(doc)),
+                                (e2) -> {
+                                    System.out.println("Caught error trying to index new UI Settings. " + e2.getMessage());
+                                    log.warn("Caught error trying to index new UI Settings", e2);
+                                    listener.onFailure(e2);
+                                }
+                            )
+                        );
+                    } else {
+                        System.out.println("Caught error when getting existing settings. " + e.getMessage());
+                        log.warn("Caught error when getting existing settings", e);
+                        listener.onFailure(e);
+                    }
+                }));
             } else {
                 // GET operation - retrieve settings
                 GetRequest getRequest = new GetRequest(request.getIndex(), configKey);
@@ -80,7 +138,13 @@ public class TransportAdvancedSettingsAction extends HandledTransportAction<Adva
                     } else {
                         listener.onFailure(new OpenSearchStatusException("Advanced settings not found", RestStatus.NOT_FOUND));
                     }
-                }, listener::onFailure));
+                }, (e) -> {
+                    if (e instanceof IndexNotFoundException) {
+                        listener.onFailure(new OpenSearchStatusException("Advanced settings not found", RestStatus.NOT_FOUND));
+                    } else {
+                        listener.onFailure(e);
+                    }
+                }));
             }
         }
     }
